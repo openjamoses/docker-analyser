386ff52f8ec03d16924f910e03d1ae29fb3df73f
openjamoses <openjamosesopm@gmail.com>
2021-11-05 11:16:15 -0400

    removed unneccessary files

--- parser/parse.py
+++
@@ -1,8 +0,0 @@
-from dockerfile_parse import DockerfileParser
-import json
-
-class ImageParser:
-    def __init__(self, src):
-        self.src = src
-    def parse(self):
-        dfp = DockerfileParser()
--- utils/constants.py
+++
@@ -1,6 +0,0 @@
-from __future__ import unicode_literals, absolute_import
-from sys import version_info
-PY2 = version_info[0] == 2
-
-DOCKERFILE_FILENAME = 'Dockerfile'
-COMMENT_INSTRUCTION = 'COMMENT'
